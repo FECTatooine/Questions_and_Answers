acb898ad1e9442a381e3f8e120f89419c4eecc5d
Symphon-y <travisredden@outlook.com>
2022-07-28 13:49:02 -0500

    ETL Complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import External Modules
 require('dotenv').config();
-import { Sequelize, Model, DataTypes } from 'sequelize';
+const { Sequelize, Model, DataTypes } = require('sequelize');
 
 // Create variables
 const user = process.env.user;
@@ -17,62 +17,81 @@ const sequelize = new Sequelize(database, user, password, {
   logging: false,
 });
 
+// Test Database Connection
+const connection = async () => {
+  console.log('attempt');
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    //console.error('Unable to connect to the database:', error);
+    let sillyworkaround = JSON.stringify(error.original.code);
+    console.log(sillyworkaround);
+    if ((sillyworkaround = '3D000')) {
+      console.log('got here');
+      sequelize.query(`create database ${database}`);
+    }
+  }
+};
+
+connection();
+
 // Create new Models
-const class Questions extends Model {}
-const class Answers extends Model {}
+// const Questions = new Model();
+// const Answers = new Model();
 
-// Create new Tables
-Questions.init({
-  question_id: {
-    type: DataTypes.BIGSERIAL,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  question_body: {
-    type: DataTypes.VARCHAR
-  },
-  question_date: {
-    type: DataTypes.TIMESTAMP
-  },
-  asker_name: {
-    type: DataTypes.VARCHAR
-  },
-  question_helpfulness: {
-    type: INTEGER
-  },
-  reported: {
-    type: DataTypes.BOOLEAN
-  },
-  answer_id: {
-    type: DataTypes.BIGSERIAL
-  }
-})
+// // Create new Tables
+// Questions.init({
+//   question_id: {
+//     type: DataTypes.BIGSERIAL,
+//     autoIncrement: true,
+//     primaryKey: true,
+//   },
+//   question_body: {
+//     type: DataTypes.VARCHAR,
+//   },
+//   question_date: {
+//     type: DataTypes.TIMESTAMP,
+//   },
+//   asker_name: {
+//     type: DataTypes.VARCHAR,
+//   },
+//   question_helpfulness: {
+//     type: INTEGER,
+//   },
+//   reported: {
+//     type: DataTypes.BOOLEAN,
+//   },
+//   answer_id: {
+//     type: DataTypes.BIGSERIAL,
+//   },
+// });
 
-Answers.init({
-  answer_id:{
-    type: DataTypes.BIGSERIAL,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  body: {
-    type: DataTypes.VARCHAR
-  },
-  date: {
-    type: DataTypes.TIMESTAMP
-  },
-  answerer_name: {
-    type: DataTypes.VARCHAR
-  },
-  helpfulness: {
-    type: DataTypes.INTEGER
-  },
-  photos: {
-    type: DataTypes.VARCHAR
-  }
-})
+// Answers.init({
+//   answer_id: {
+//     type: DataTypes.BIGSERIAL,
+//     autoIncrement: true,
+//     primaryKey: true,
+//   },
+//   body: {
+//     type: DataTypes.VARCHAR,
+//   },
+//   date: {
+//     type: DataTypes.TIMESTAMP,
+//   },
+//   answerer_name: {
+//     type: DataTypes.VARCHAR,
+//   },
+//   helpfulness: {
+//     type: DataTypes.INTEGER,
+//   },
+//   photos: {
+//     type: DataTypes.VARCHAR,
+//   },
+// });
 
-  // Add Foreign Key to Answers Table (one to many relationship)
-Answers.belongsTo(Questions, {
-  foreignKey: "question_id",
-  targetKey:  "question_id",
-})
+// // Add Foreign Key to Answers Table (one to many relationship)
+// Answers.belongsTo(Questions, {
+//   foreignKey: 'question_id',
+//   targetKey: 'question_id',
+// });
---
+++ schema.sql
@@ -0,0 +1,64 @@
+psql questions_and_answers;
+
+
+CREATE TABLE IF NOT EXISTS questions(
+  question_id bigserial PRIMARY KEY,
+  product_id bigserial,
+  question_body varchar,
+  question_date bigint,
+  asker_name varchar,
+  asker_email varchar,
+  reported BOOLEAN,
+  question_helpfulness INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS answers(
+  answer_id bigserial PRIMARY KEY,
+  question_id bigint,
+  question_body varchar,
+  question_date bigint,
+  answerer_name varchar,
+  answerer_email varchar,
+  reported BOOLEAN,
+  question_helpfulness INTEGER,
+  FOREIGN KEY(question_id)
+	  REFERENCES questions(question_id)
+);
+
+CREATE TABLE IF NOT EXISTS answer_photos(
+photo_id bigserial PRIMARY KEY,
+answer_id bigint,
+url varchar,
+FOREIGN KEY(answer_id)
+	  REFERENCES answers(answer_id)
+);
+
+-- Filepaths will need to be corrected to relative path
+COPY questions FROM '/Users/travisredden/Documents/Software Engineering/HackReactor/SDC/Questions_and_Answers/Assets/Raw_CSV/questions.csv' CSV
+HEADER;
+
+COPY answers FROM '/Users/travisredden/Documents/Software Engineering/HackReactor/SDC/Questions_and_Answers/Assets/Raw_CSV/answers.csv' CSV
+HEADER;
+
+COPY answer_photos FROM '/Users/travisredden/Documents/Software Engineering/HackReactor/SDC/Questions_and_Answers/Assets/Raw_CSV/answers_photos.csv' CSV
+HEADER;
+
+-- Templates
+
+-- FOREIGN KEY(customer_id)
+-- 	  REFERENCES customers(customer_id)
+
+-- CREATE TABLE [IF NOT EXISTS] table_name (
+--    column1 datatype(length) column_contraint,
+--    column2 datatype(length) column_contraint,
+--    column3 datatype(length) column_contraint,
+--    table_constraints
+-- );
+
+
+-- id	product_id	body	date_written	asker_name	asker_email	reported	helpful
+
+-- COPY table_name [ ( column_name [, ...] ) ]
+--     FROM { 'filename' | PROGRAM 'command' | STDIN }
+--     [ [ WITH ] ( option [, ...] ) ]
+--     [ WHERE condition ]
